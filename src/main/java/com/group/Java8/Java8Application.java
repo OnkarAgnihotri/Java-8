2c81e843721dbfc5c0388a3079aa63abdeb4fdd9
Onkar Agnihotri <onkaragnihotri9@gmail.com>
2024-02-09 08:29:12 +0530

    Master : Working Application

--- src/main/java/com/group/Java8/Java8Application.java
+++ src/main/java/com/group/Java8/Java8Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Java8Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Java8Application.class, args);
+		System.out.println("Hello Java8...!");
+//		SpringApplication.run(Java8Application.class, args);
 	}
 
 }
