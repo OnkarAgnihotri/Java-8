d220ded8660c157c7494468d7cda49883701787c
Onkar Agnihotri <onkaragnihotri9@gmail.com>
2024-02-11 18:38:18 +0530

    Predicate_1: Student class added

--- src/main/java/com/group/Java8/Java8Application.java
+++ src/main/java/com/group/Java8/Java8Application.java
@@ -1,14 +1,15 @@
 package com.group.Java8;
 
-import org.springframework.boot.SpringApplication;
+import com.group.Java8.Model.Student;
+import com.group.Java8.predicate.Predicate;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Java8Application {
-
-	public static void main(String[] args) {
-		System.out.println("Hello Java8...!");
-//		SpringApplication.run(Java8Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        Student student = new Student();
+        com.group.Java8.predicate.Predicate predicate = new Predicate();
+        predicate.isAgeGt21andGenderFemale(student.getStudentList());
+        predicate.isBeetween(20);
+    }
 }
